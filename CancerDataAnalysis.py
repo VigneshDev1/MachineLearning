ae5295d89e63c545fa74dd8833b0bae261e78a26
VigneshDev1 <vignesh.dev91@gmail.com>
2021-04-30 07:42:06 -0600

    Introduction to ML Project

--- CancerDataAnalysis.py
+++ CancerDataAnalysis.py
@@ -10,10 +10,11 @@ import pandas as pd
 cancer_data = load_breast_cancer()
 
 print(cancer_data.keys())
-
+otherinfo=[]
 for i in cancer_data.keys():
     try:
         print("{0} has shape {1}".format(i, cancer_data[i].shape))
     except:
-        print(cancer_data[i])
-cancer_data['DESCR']
+        if cancer_data[i] is not None:
+            otherinfo.append(i + " [*******] " + cancer_data[i])
+
